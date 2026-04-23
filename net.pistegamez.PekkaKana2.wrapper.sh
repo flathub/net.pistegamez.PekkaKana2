a6a39ce96fd59386da68464a2981f95e2331d681
SaturninTheAlien <alienufobomber@gmail.com>
2026-04-24 00:12:20 +0200

    Update to 1.5.2

---
+++ net.pistegamez.PekkaKana2.wrapper.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+DATA_OLD="$(dirname "$XDG_DATA_HOME")/.pekka-kana-2"
+echo $DATA_OLD
+
+DATA_NEW=$XDG_DATA_HOME
+
+# migrate saves.dat
+if [ ! -f "$DATA_NEW/saves.dat" ] && [ -f "$DATA_OLD/saves.dat" ]; then
+    mkdir -p "$DATA_NEW"
+    cp -a "$DATA_OLD/saves.dat" "$DATA_NEW/"
+fi
+
+# migrate scores
+SCORES_NEW="$DATA_NEW/scores"
+if [ ! -d "$SCORES_NEW" ] && [ -d "$DATA_OLD" ]; then
+    mkdir -p "$SCORES_NEW"
+
+    for dir in "$DATA_OLD"/*; do
+        [ -d "$dir" ] || continue
+
+        OLD_FILE="$dir/scores.dat"
+        [ -f "$OLD_FILE" ] || continue
+
+        BASENAME="$(basename "$dir")"
+
+        # change "-" to spaces
+
+        NAME="$(echo "$BASENAME" | tr '-' ' ')"
+        NEW_FILE="$SCORES_NEW/$NAME.dat"
+
+        if [ ! -f "$NEW_FILE" ]; then
+            cp -a "$OLD_FILE" "$NEW_FILE"
+        fi
+    done
+fi
+
+
+
+exec /app/share/games/pekka-kana-2/pekka-kana-2 \
+    --assets-path /app/share/games/pekka-kana-2 \
+    --data-path "$DATA_NEW"
